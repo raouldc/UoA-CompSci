c5380ed5f2aec7166fa1ee4fa6a7eea3c56cf4d2
Raoul <raoul_dcunha@hotmail.com>
2013-09-15 00:51:52 +1200

    made staff class parcelable

--- src/com/raouldc/uoacompsci/Staff.java
+++ src/com/raouldc/uoacompsci/Staff.java
@@ -3,8 +3,10 @@ package com.raouldc.uoacompsci;
 import java.io.Serializable;
 
 import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Staff implements Comparable<Staff>,Serializable{
+public class Staff implements Comparable<Staff>,Serializable,Parcelable{
 
 	//Vcard fields: Tel(Work and Fax), Name, Fullname,Address, Title, email, organisation, url, photo(jpg)
 	private String _name, _fullname,_upi, _tel_work, _tel_fax, _email, _organisation,  _url, _address;
@@ -33,6 +35,11 @@ public class Staff implements Comparable<Staff>,Serializable{
 		this._url = _url;
 		this._photo = _photo;
 		this._address =_address;
+	}
+	
+	public Staff(Parcel in)
+	{
+		readFromParcel(in);
 	}
 	
 
@@ -70,4 +77,54 @@ public class Staff implements Comparable<Staff>,Serializable{
 		return this._name.toLowerCase().compareTo(arg0._name.toLowerCase());
 	}
 
+
+	@Override
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public void writeToParcel(Parcel arg0, int arg1) {
+		// TODO Auto-generated method stub
+		arg0.writeString(_name);
+		arg0.writeString(_fullname);
+		arg0.writeString(_upi);
+		arg0.writeString(_tel_work);
+		arg0.writeString(_tel_fax);
+		arg0.writeString(_email);
+		arg0.writeString(_organisation);
+		arg0.writeString(_url);
+		arg0.writeString(_address);
+		arg0.writeByteArray(_photo);
+	}
+	
+	private void readFromParcel(Parcel in) {
+		 
+		// We just need to read back each
+		// field in the order that it was
+		// written to the parcel
+		_name = in.readString();
+		_fullname = in.readString();
+		_upi = in.readString();
+		_tel_work= in.readString();
+		_tel_fax= in.readString();
+		_email= in.readString();
+		_organisation= in.readString();
+		_url= in.readString();
+		_address= in.readString();
+		in.readByteArray(_photo);
+	}
+	
+	public static final Parcelable.Creator CREATOR =
+	    	new Parcelable.Creator() {
+	            public Staff createFromParcel(Parcel in) {
+	                return new Staff(in);
+	            }
+	 
+	            public Staff[] newArray(int size) {
+	                return new Staff[size];
+	            }
+	        };
 }
