bdb6b600971a5c92e5c606ab51d48f27f8779ab6
Raoul <raoul_dcunha@hotmail.com>
2013-09-15 20:26:11 +1200

    got the item click handlers working

--- src/com/raouldc/uoacompsci/StaffDetailActivity.java
+++ src/com/raouldc/uoacompsci/StaffDetailActivity.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.OptionalDataException;
 import java.io.StreamCorruptedException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import android.os.Bundle;
 import android.app.ActionBar;
@@ -19,6 +20,9 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -27,15 +31,54 @@ import android.widget.TextView;
 import android.support.v4.app.NavUtils;
 
 public class StaffDetailActivity extends Activity {
-	private String name,address,email,url,phone;
+	private String name, address, email, url, phone;
 	private byte[] photo;
+	private ListView phonelv;
+	private ListView addresslv;
+	private ListView emaillv;
+	private ListView urllv;
+
+	/* Item click listener for the list1 item click event. */
+	private transient OnItemClickListener list1ClickListener = new OnItemClickListener() {
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * android.widget.AdapterView.OnItemClickListener#onItemClick(android
+		 * .widget.AdapterView, android.view.View, int, long)
+		 */
+
+		@Override
+		public void onItemClick(final AdapterView<?> parent, final View view,
+				final int position, final long itemID) {
+		}
+	};
+
+	/* Item click listener for the list2 item click event. */
+	private transient OnItemClickListener list2ClickListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(final AdapterView<?> parent, final View view,
+				final int position, final long itemID) {
+		}
+	};
+
+	/* Item click listener for the list3 item click event. */
+	private transient OnItemClickListener list3ClickListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(final AdapterView<?> parent, final View view,
+				final int position, final long itemID) {
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_staff_detail);
 		// Show the Up button in the action bar.
-		
-		
+
 		Bundle b = getIntent().getExtras();
 		name = b.getString("name");
 		address = b.getString("address");
@@ -43,22 +86,33 @@ public class StaffDetailActivity extends Activity {
 		photo = b.getByteArray("photo");
 		url = b.getString("url");
 		phone = b.getString("phone");
-		
+
 		//
 		Bitmap bm = BitmapFactory.decodeByteArray(photo, 0, photo.length);
-		((ImageView)findViewById(R.id.staffDetailPic)).setImageBitmap(bm);
-		
-		
-		StaffDetailListView lv = new StaffDetailListView(R.id.phonelistView1);
-		
-		
-		
-		
-		
-		
+		((ImageView) findViewById(R.id.staffDetailPic)).setImageBitmap(bm);
 
-//		TextView tw = (TextView) findViewById(R.id.staffName);
-//		tw.setText(phone);
+		// StaffDetailListView lv = new
+		// StaffDetailListView(R.id.phonelistView1);
+		phonelv = (ListView) findViewById(R.id.phonelistView1);
+		addresslv = (ListView) findViewById(R.id.addressListView1);
+		emaillv = (ListView) findViewById(R.id.emailListView1);
+		urllv = (ListView) findViewById(R.id.websiteListView1);
+
+		phonelv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new ArrayList<String>(
+						Arrays.asList(phone))));
+		addresslv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new ArrayList<String>(
+						Arrays.asList(address))));
+		emaillv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new ArrayList<String>(
+						Arrays.asList(email))));
+		urllv.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, new ArrayList<String>(
+						Arrays.asList(url))));
+		
+		phonelv.setOnItemClickListener(list1ClickListener);
+
 		setupActionBar();
 	}
 
@@ -95,47 +149,5 @@ public class StaffDetailActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-	
-	
-	public class StaffDetailListView extends ListActivity {
-	    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
-	    ArrayList<String> listItems=new ArrayList<String>();
-
-	    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-	    ArrayAdapter<String> adapter;
-
-	    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
-	    int clickCounter=0;
-	    int _id;
-	    
-	    public StaffDetailListView(int id)
-	    {
-	    	_id = id;
-	    }
-
-	    @Override
-	    public void onCreate(Bundle icicle) {
-	        super.onCreate(icicle);
-	        setContentView(R.layout.activity_staff_detail);
-	        listItems.add("Clicked : "+clickCounter++);
-	        adapter=new ArrayAdapter<String>(this,
-	            _id,
-	            listItems);
-	        setListAdapter(adapter);
-	    }
-
-	    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
-	    public void addItems(View v) {
-	        listItems.add("Clicked : "+clickCounter++);
-	        adapter.notifyDataSetChanged();
-	    }
-	    
-	    @Override
-	    protected void onListItemClick(ListView l, View v, int position, long id) {
-	     // TODO Auto-generated method stub
-	     //super.onListItemClick(l, v, position, id);
-	    	addItems(v);
-	    }
 	}
 }
