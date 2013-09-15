7c3dc4f1413d4d7b9f4ee3c59464aa51d3258975
Raoul <raoul_dcunha@hotmail.com>
2013-09-15 19:41:13 +1200

    created a custom adapter for each list

--- src/com/raouldc/uoacompsci/StaffDetailActivity.java
+++ src/com/raouldc/uoacompsci/StaffDetailActivity.java
@@ -12,9 +12,16 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ListActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.support.v4.app.NavUtils;
@@ -38,9 +45,20 @@ public class StaffDetailActivity extends Activity {
 		phone = b.getString("phone");
 		
 		//
+		Bitmap bm = BitmapFactory.decodeByteArray(photo, 0, photo.length);
+		((ImageView)findViewById(R.id.staffDetailPic)).setImageBitmap(bm);
+		
+		
+		StaffDetailListView lv = new StaffDetailListView(R.id.phonelistView1);
+		
+		
+		
+		
+		
+		
 
-		TextView tw = (TextView) findViewById(R.id.staffName);
-		tw.setText(phone);
+//		TextView tw = (TextView) findViewById(R.id.staffName);
+//		tw.setText(phone);
 		setupActionBar();
 	}
 
@@ -78,5 +96,46 @@ public class StaffDetailActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	public class StaffDetailListView extends ListActivity {
+	    //LIST OF ARRAY STRINGS WHICH WILL SERVE AS LIST ITEMS
+	    ArrayList<String> listItems=new ArrayList<String>();
 
+	    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+	    ArrayAdapter<String> adapter;
+
+	    //RECORDING HOW MANY TIMES THE BUTTON HAS BEEN CLICKED
+	    int clickCounter=0;
+	    int _id;
+	    
+	    public StaffDetailListView(int id)
+	    {
+	    	_id = id;
+	    }
+
+	    @Override
+	    public void onCreate(Bundle icicle) {
+	        super.onCreate(icicle);
+	        setContentView(R.layout.activity_staff_detail);
+	        listItems.add("Clicked : "+clickCounter++);
+	        adapter=new ArrayAdapter<String>(this,
+	            _id,
+	            listItems);
+	        setListAdapter(adapter);
+	    }
+
+	    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
+	    public void addItems(View v) {
+	        listItems.add("Clicked : "+clickCounter++);
+	        adapter.notifyDataSetChanged();
+	    }
+	    
+	    @Override
+	    protected void onListItemClick(ListView l, View v, int position, long id) {
+	     // TODO Auto-generated method stub
+	     //super.onListItemClick(l, v, position, id);
+	    	addItems(v);
+	    }
+	}
 }
